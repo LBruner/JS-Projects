c849c0d3625b5e48a8d0ee72207758e8c06deb70
LBruner <leandrobruner15@gmail.com>
2021-12-22 09:58:56 -0300

    start screen check for infinite scroll

--- InfiniteScroll/script.js
+++ InfiniteScroll/script.js
@@ -1,17 +1,56 @@
-const imageCount = 4;
+const imageContainer = document.querySelector('#image-container');
+const loader = document.querySelector('#loader');
+
+
+const imageCount = 3;
 const apiKey = 'OgAQ6-8y5HfoySxLQB4ALafTyU6Ay_slat1a7FKgclc';
-const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${imageCount}`;
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${imageCount}&orientation=squarish`;
+
+photosArray = [];
 
 async function getPhotos(){
     try{
-        let response = await fetch(apiUrl);
-        const data = response.json();
-        console.log(data);
+        const response = await fetch(apiUrl);
+        photosArray = await response.json();
+        console.log(photosArray)
+        createImageElement();
     }
     catch (e) {
         console.log(e);
     }
 }
+
+function setAttributes(element, attributes){
+    for (const key in attributes){
+        element.setAttribute(key, attributes[key]);
+    }
+}
+
+function createImageElement() {
+    photosArray.forEach((photo) =>{
+        const item = document.createElement('a');
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank'
+        })
+        const img = document.createElement('img');
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description
+        })
+        img.src = photo.urls.regular;
+        item.appendChild(img);
+        
+        imageContainer.appendChild(item);
+    })
+}
+
+window.addEventListener('scroll', ()=>{
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 100){
+        getPhotos();
+    }
+})
 
 getPhotos();
 
